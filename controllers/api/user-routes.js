490765cc64183273d5bc234fb9ece1824f4628cc
JessicaAKelley <kelleyajessica@gmail.com>
2021-10-01 10:55:28 -0500

    Models for User, Post, and Comment; API routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+
+const {
+    User,
+    Post
+} = require("../../models");
+
+// Get all users
+router.get("/", (req, res) => {
+    User.findAll({
+            attributes: {
+                exclude: ["password"]
+            }
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Create a user
+router.post("/", (req, res) => {
+    User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
